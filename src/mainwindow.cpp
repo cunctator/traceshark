55b773ff6b6667a527ac1b7c29ee6e986b19728b
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2015-08-30 10:43:08 +0300

    MainWindow: Remove silly header
    I feel that this header is superfluous now.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -462,7 +462,6 @@ void MainWindow::about()
 	       "<p>This is version %1.</p>"
 		).arg(QLatin1String(TRACESHARK_VERSION_STRING));
 	textAbout = QMessageBox::tr(
-	       "<h1>Traceshark</h1>"
 	       "<p>Copyright &copy; 2014, 2015 Viktor Rosendahl"
 	       "<p>This program comes with ABSOLUTELY NO WARRANTY; details below."
 	       "<p>This is free software, and you are welcome to redistribute it"
