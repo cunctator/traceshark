cf85a87124c65b0c9f23312e0ed57c4cda4767aa
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2015-11-15 23:18:18 +0200

    Tune copyright message
    Now when this is a tool for perf traces, let it show in the copyright
    messages too!
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- src/taskgraph.cpp
+++ src/taskgraph.cpp
@@ -1,5 +1,5 @@
 /*
- * Traceshark - a visualizer for visualizing ftrace traces
+ * Traceshark - a visualizer for visualizing ftrace and perf traces
  * Copyright (C) 2015  Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  *   This program is free software: you can redistribute it and/or modify
