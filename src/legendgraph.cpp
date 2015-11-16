f9f04e00640f3267b7dbb41e19070f35d9bcfbd5
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2015-11-16 23:31:44 +0200

    LegendGraph: make compile warning go away
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- src/legendgraph.cpp
+++ src/legendgraph.cpp
@@ -20,7 +20,7 @@
 
 LegendGraph::LegendGraph(QCPAxis *keyAxis, QCPAxis *valueAxis,
 		       QCPAbstractPlottable *p):
-	QCPGraph(keyAxis, valueAxis), plottable(p), pid(0)
+	QCPGraph(keyAxis, valueAxis), pid(0), plottable(p)
 {}
 
 QCPAbstractPlottable *LegendGraph::getPlottable()
