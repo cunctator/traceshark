cunctator/traceshark

commit 46cb72ab3c6c5ed98937d613686d94b4175a104e

MainWindow: run setupCursors() before showTrace()
The order of events are important for reasons of performance if you
load a very large trace. This is because the setting up (and moving)
of cursors triggers a trace replot and if the trace is already filled
with plenty of things, then it will be slow. However, if you do it
before the trace has been cluttered with graphs, then it's almost
instant. With this change, for a 720 MB trace, the time to run
setupCursors() goes from about 2.8s to about  0.0020s.

Before, the timings looks like this:
opening /home/viktor/git/traces/medium.asc
Loading took 0.001000 s
61162295
processTrace() took 3.288000 s
computeLayout() took 0.000000 s
rescaleTrace() took 0.018000 s
showTrace() took 0.656000 s
updating EventsWidget took 0.002000 s
setupCursors() took 2.765000 s
tracePlot->show() took 0.718000 s

..and after:
opening /home/viktor/mygit/traces/medium.asc
Loading took 0.002000 s
61162295
processTrace() took 3.135000 s
computeLayout() took 0.001000 s
setupCursors() took 0.002000 s
rescaleTrace() took 0.017000 s
showTrace() took 0.653000 s
updating EventsWidget took 0.002000 s
tracePlot->show() took 0.729000 s

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

src/mainwindow.cpp
@@ -143,6 +143,9 @@ void MainWindow::openTrace()
 		computeLayout();
 		layout = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
 
+		setupCursors();
+		scursor = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
+
 		rescaleTrace();
 		rescale = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
 
@@ -154,9 +157,6 @@ void MainWindow::openTrace()
 		eventsWidget->endResetModel();
 		eventsw = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
 
-		setupCursors();
-		scursor = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
-
 		tracePlot->show();
 		tshow = QDateTime::currentDateTimeUtc().toMSecsSinceEpoch();
 
@@ -166,8 +166,11 @@ void MainWindow::openTrace()
 		qout << "computeLayout() took "
 		     << (double) (layout - process) / 1000;
 		qout << " s\n";
+		qout << "setupCursors() took "
+		     << (double) (scursor - process) / 1000;
+		qout << " s\n";
 		qout << "rescaleTrace() took "
-		     << (double) (rescale - layout) / 1000;
+		     << (double) (rescale - scursor) / 1000;
 		qout << " s\n";
 		qout << "showTrace() took "
 		     << (double) (showt - rescale) / 1000;
@@ -175,11 +178,8 @@ void MainWindow::openTrace()
 		qout << "updating EventsWidget took "
 		     << (double) (eventsw - showt) / 1000;
 		qout << " s\n";
-		qout << "setupCursors() took "
-		     << (double) (scursor - eventsw) / 1000;
-		qout << " s\n";
 		qout << "tracePlot->show() took "
-		     << (double) (tshow - scursor) / 1000;
+		     << (double) (tshow - eventsw) / 1000;
 		qout << " s\n";
 		tracePlot->legend->setVisible(true);
 	}

commit bd78cd9b313d6fabf3b6202c0d857cd95373ca7f

MainWindow: Fix third regression from moving setupCursors()
Unfortunately, I discovered a third regression from commit
46cb72ab3c6c5ed98937d613686d94b4175a104e. This is caused by
the fact that the setupCursor() code calls Cursor::setPosition()
which calls TracePlot::replot(). Earlier this was done after
MainWindow::showTrace(), so it was guaranteed that there
would be a replot after the graphs had been added to the plot. This
is no longer true, so the solution is to add a replot() call
at the end of MainWindow::showTrace()

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

src/mainwindow.cpp
@@ -351,6 +351,7 @@ void MainWindow::showTrace()
 			addStillRunningGraph(task);
 		}
 	}
+	tracePlot->replot();
 }
 
 void MainWindow::setupCursors()