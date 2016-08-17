a89fcf2e8f5a19d0a1e38e653117977c05a70289
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2016-08-18 02:55:40 +0300

    Release version 0.2.1-alpha
    There is only one new thing in this release:
    
    * Now the MainWindow start size is calculated from available geometry
    with the help of a heuristic. I feel that this is much better than
    using hard coded numbers.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- misc/traceshark.h
+++ misc/traceshark.h
@@ -19,7 +19,7 @@
 #ifndef TRACESHARK_H
 #define TRACESHARK_H
 
-#define TRACESHARK_VERSION_STRING "0.2.0-alpha"
+#define TRACESHARK_VERSION_STRING "0.2.1-alpha"
 #define QCUSTOMPLOT_VERSION_STRING "1.3.2"
 
 #include <QtCore>
