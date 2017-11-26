a10e8cd8d114376bfeae0ea6d1f62e128fb8d822
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2017-11-26 03:08:14 +0100

    Fix whitespace errors
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -2,31 +2,31 @@
 #
 #  testrun.sh - a script to launch traceshark multiple times
 #  Copyright (C) 2017  Viktor Rosendahl <viktor.rosendahl@gmail.com>
-# 
+#
 #  This file is dual licensed: you can use it either under the terms of
 #  the GPL, or the BSD license, at your option.
-# 
+#
 #   a) This program is free software; you can redistribute it and/or
 #      modify it under the terms of the GNU General Public License as
 #      published by the Free Software Foundation; either version 2 of the
 #      License, or (at your option) any later version.
-# 
+#
 #      This program is distributed in the hope that it will be useful,
 #      but WITHOUT ANY WARRANTY; without even the implied warranty of
 #      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #      GNU General Public License for more details.
-# 
+#
 #      You should have received a copy of the GNU General Public
 #      License along with this library; if not, write to the Free
 #      Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 #      MA 02110-1301 USA
-# 
+#
 #  Alternatively,
-# 
+#
 #   b) Redistribution and use in source and binary forms, with or
 #      without modification, are permitted provided that the following
 #      conditions are met:
-# 
+#
 #      1. Redistributions of source code must retain the above
 #         copyright notice, this list of conditions and the following
 #         disclaimer.
@@ -34,7 +34,7 @@
 #         copyright notice, this list of conditions and the following
 #         disclaimer in the documentation and/or other materials
 #         provided with the distribution.
-# 
+#
 #      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
 #      CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 #      INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
