dda1195a038c75d33869f854118d66028f5039ac
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2018-10-04 23:23:52 +0200

    Release 0.5.0-alpha
    This release contains an important new feature:
    - The button to find sched_waking events that precedes a specific
      sched_wakeup event.
    
    There are two bug fixes:
    - The cursor buttons and time fields where not enabled when a trace
      was opened.
    - The events widget did not stay in the same place after a filter
      was disabled.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- misc/traceshark.h
+++ misc/traceshark.h
@@ -52,7 +52,7 @@
 #ifndef TRACESHARK_H
 #define TRACESHARK_H
 
-#define TRACESHARK_VERSION_STRING "0.4.3-alpha"
+#define TRACESHARK_VERSION_STRING "0.5.0-alpha"
 
 #include <QtCore>
 #include <cstdint>
