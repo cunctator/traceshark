23e60bdd06c0eb816eefdf4d79b38d69ba77169d
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2018-11-25 22:11:45 +0100

    Release 0.6.0-alpha
    This release contains an important new feature:
        - The button to find sched_waking events directly, based on the
          currently selected task and active cursor position. With this
          button it's not necessary to first find the wakeup event.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- misc/traceshark.h
+++ misc/traceshark.h
@@ -52,7 +52,7 @@
 #ifndef TRACESHARK_H
 #define TRACESHARK_H
 
-#define TRACESHARK_VERSION_STRING "0.5.1-alpha"
+#define TRACESHARK_VERSION_STRING "0.6.0-alpha"
 
 #include <QtCore>
 #include <cstdint>
