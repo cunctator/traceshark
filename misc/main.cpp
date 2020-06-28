d0fc626b405d64139eac2927d2f6ec71bc9f3313
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2020-06-28 21:47:59 +0200

    main(): Add Qt::AA_UseHighDpiPixmaps
    I believe that it perhaps is better to add this option to get high dpi
    icons. I am not sure whether the icons will look better or not, since
    the icon pixmap handling is done by Qt rather than the application
    code. Also, I don't have any HiDPI system to test it with.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- misc/main.cpp
+++ misc/main.cpp
@@ -100,6 +100,9 @@ int main(int argc, char* argv[])
 #if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+#if QT_VERSION >= QT_VERSION_CHECK(5,1,0)
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
 
 	QApplication app(argc, argv);
 	MainWindow mainWindow;
