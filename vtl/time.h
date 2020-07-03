cunctator/traceshark

commit 75ffc72e3cefb821138f489d0441a4eb24a90d2c

Standardize usage of underscore
According to the C++ standard, the following identifiers are reserved
by the implementation:
- identifiers that contain a double underscore.
- identiferrs that begin with an underscore followed by an uppercase
  letter.
- identifiers in the global namespace that begin with an underscore.

For this reason I have decided to:
- Decrease the usage of underscore. A lot of the current usage
  isn't very helpful.
- In those places were I want to express caution, I will replace
  __foo() and _foo() with foo_().
- I would be possible to use namespace::_foo() but since I think it's
  better to have more uniform rules, I will use namespace::foo_().

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

vtl/time.h
@@ -50,8 +50,8 @@
  *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _VTL_TIME_H
-#define _VTL_TIME_H
+#ifndef VTL_TIME_H
+#define VTL_TIME_H
 
 #include <QString>
 #include <climits>
@@ -65,44 +65,44 @@ namespace vtl {
 #define VTL_TIME_MIN vtl::Time(true, UINT_MAX, UINT_MAX)
 #define VTL_TIME_ZERO vtl::Time(false, 0, 0)
 
-#define _TIME_MAX(A, B) (A > B ? A:B)
-#define _TIME_MIN(A, B) (A < B ? A:B)
+#define VTL_TIME_MAX_(A, B) (A > B ? A:B)
+#define VTL_TIME_MIN_(A, B) (A < B ? A:B)
 
-#define _TIMEINT_REQ (1000000000000000000LL)
+#define VTL_TIMEINT_REQ_ (1000000000000000000LL)
 
-#if INT_MAX >= _TIMEINT_REQ && INT_MIN < -_TIMEINT_REQ
-#define	_TIME_MILLE (1000)
-#define TIME_USE_INT
-#define _TIME_FMT_STRING  "%d"
+#if INT_MAX >= VTL_TIMEINT_REQ_ && INT_MIN < -VTL_TIMEINT_REQ_
+#define	VTL_TIME_MILLE_ (1000)
+#define VTL_TIME_USE_INT
+#define VTL_TIME_FMT_STRING_ "%d"
 
 #elif LONG_MAX >= _TIMEINT_REQ && LONG_MIN < -_TIMEINT_REQ
-#define	_TIME_MILLE (1000L)
-#define TIME_USE_LONG
-#define _TIME_FMT_STRING  "%ld"
+#define	VTL_TIME_MILLE_ (1000L)
+#define VTL_TIME_USE_LONG
+#define VTL_TIME_FMT_STRING_  "%ld"
 
 #elif LLONG_MAX >= _TIMEINT_REQ && LLONG_MIN < -_TIMEINT_REQ
-#define	_TIME_MILLE (1000LL)
-#define TIME_USE_LLONG
-#define _TIME_FMT_STRING  "%lld"
+#define	VTL_TIME_MILLE_ (1000LL)
+#define VTL_TIME_USE_LLONG
+#define VTL_TIME_FMT_STRING_  "%lld"
 
 #else
 #error "A longer long long is required!"
 #endif
 
-#define USECS_PER_MSEC (_TIME_MILLE)
-#define NSECS_PER_USEC (_TIME_MILLE)
-#define MSECS_PER_SEC (_TIME_MILLE)
+#define USECS_PER_MSEC (VTL_TIME_MILLE_)
+#define NSECS_PER_USEC (VTL_TIME_MILLE_)
+#define MSECS_PER_SEC (VTL_TIME_MILLE_)
 #define USECS_PER_SEC (MSECS_PER_SEC * USECS_PER_MSEC)
 #define NSECS_PER_SEC (USECS_PER_SEC * NSECS_PER_USEC)
 
 	typedef class Time final {
 	public:
 
-#ifdef TIME_USE_INT
+#ifdef VTL_TIME_USE_INT
 		typedef int timeint_t;
-#elif defined(TIME_USE_LONG)
+#elif defined(VTL_TIME_USE_LONG)
 		typedef long timeint_t;
-#elif defined(TIME_USE_LLONG)
+#elif defined(VTL_TIME_USE_LLONG)
 		typedef long long timeint_t;
 #endif
 	Time(bool n = false, timeint_t s = 0, timeint_t ns = 0,
@@ -140,10 +140,10 @@ namespace vtl {
 		vtl_always_inline unsigned int getPrecision() const;
 		vtl_always_inline void setPrecision(unsigned int p);
 	private:
-		vtl_always_inline static Time __fromString(const char *str,
-							 bool &ok,
-							 bool spaced,
-							 bool colonatend);
+		vtl_always_inline static Time fromString_(const char *str,
+							  bool &ok,
+							  bool spaced,
+							  bool colonatend);
 		timeint_t time;
 		unsigned int precision : 4;
 	} time_t;
@@ -151,28 +151,28 @@ namespace vtl {
 	vtl_always_inline Time Time::operator+(const Time &other) const
 	{
 		Time r;
-		r.precision = _TIME_MAX(precision, other.precision);
+		r.precision = VTL_TIME_MAX_(precision, other.precision);
 		r.time = time + other.time;
 		return r;
 	}
 
 	vtl_always_inline void Time::operator+=(const Time &other)
 	{
-		precision = _TIME_MAX(precision, other.precision);
+		precision = VTL_TIME_MAX_(precision, other.precision);
 		time = time + other.time;
 	}
 
 	vtl_always_inline Time Time::operator-(const Time &other) const
 	{
 		Time r;
-		r.precision = _TIME_MAX(precision, other.precision);
+		r.precision = VTL_TIME_MAX_(precision, other.precision);
 		r.time = time - other.time;
 		return r;
 	}
 
 	vtl_always_inline void Time::operator-=(const Time &other)
 	{
-		precision = _TIME_MAX(precision, other.precision);
+		precision = VTL_TIME_MAX_(precision, other.precision);
 		time = time - other.time;
 	}
 
@@ -266,12 +266,12 @@ namespace vtl {
 
 	vtl_always_inline Time Time::fromString(const char *str, bool &ok)
 	{
-		return __fromString(str, ok, false, true);
+		return fromString_(str, ok, false, true);
 	}
 
 	vtl_always_inline Time Time::fromSpacedString(const char *str, bool &ok)
 	{
-		return __fromString(str, ok, true, false);
+		return fromString_(str, ok, true, false);
 	}
 
 	vtl_always_inline bool Time::isZero()
@@ -279,8 +279,8 @@ namespace vtl {
 		return time == 0;
 	}
 
-	vtl_always_inline Time Time::__fromString(const char *str, bool &ok,
-						bool spaced, bool colonatend)
+	vtl_always_inline Time Time::fromString_(const char *str, bool &ok,
+						 bool spaced, bool colonatend)
 	{
 		Time r;
 		uint32_t base = 0;
@@ -434,4 +434,4 @@ namespace vtl {
 	}
 }
 
-#endif /* _VTL_TIME_H */
+#endif /* VTL_TIME_H */

analyzer/abstracttask.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2015, 2016, 2018, 2019
+ * Copyright (C) 2015, 2016, 2018-2020
  * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
@@ -321,8 +321,8 @@ void AbstractTask::setEndTime(const vtl::Time &time)
 	endTime = time;
 }
 
-int AbstractTask::_binarySearch(const vtl::Time &time, int lowerIdx,
-			       int higherIdx)
+int AbstractTask::binarySearch_(const vtl::Time &time, int lowerIdx,
+				int higherIdx)
 {
 	int pivot = (lowerIdx + higherIdx) / 2;
 	int width = higherIdx - lowerIdx;
@@ -336,21 +336,21 @@ int AbstractTask::_binarySearch(const vtl::Time &time, int lowerIdx,
 	lowerIdx = pSmaller ? pivot : lowerIdx;
 	higherIdx = pSmaller ? higherIdx : pivot;
 
-	return _binarySearch(time, lowerIdx, higherIdx);
+	return binarySearch_(time, lowerIdx, higherIdx);
 }
 
 int AbstractTask::binarySearch(const vtl::Time &time)
 {
 	int s = schedEventIdx.size();
 
-	return _binarySearch(time, 0, s - 1);
+	return binarySearch_(time, 0, s - 1);
 }
 
 
 int AbstractTask::findLower(const vtl::Time &time)
 {
 	int idxmax = schedEventIdx.size() - 1;
-	int idx = _binarySearch(time, 0, idxmax);
+	int idx = binarySearch_(time, 0, idxmax);
 
 	vtl::Time idxtime = (*events)[schedEventIdx[idx]].time;
 	/*
@@ -370,7 +370,7 @@ int AbstractTask::findLower(const vtl::Time &time)
 int AbstractTask::findHigher(const vtl::Time &time)
 {
 	int idxmax = schedEventIdx.size() - 1;
-	int idx = _binarySearch(time, 0, idxmax);
+	int idx = binarySearch_(time, 0, idxmax);
 
 	vtl::Time idxtime = (*events)[schedEventIdx[idx]].time;
 	/*

analyzer/abstracttask.h
@@ -125,8 +125,7 @@ public:
 	TaskGraph *graph;
 
 private:
-	int _binarySearch(const vtl::Time &time, int lowerIdx,
-					  int higherIdx);
+	int binarySearch_(const vtl::Time &time, int lowerIdx, int higherIdx);
 	vtl_always_inline int binarySearch(const vtl::Time &time);
 	int findLower(const vtl::Time &time);
 	int findHigher(const vtl::Time &time);

analyzer/traceanalyzer.cpp
@@ -85,7 +85,7 @@ extern "C" {
 #include "threads/workqueue.h"
 
 vtl_always_inline static int clib_open(const char *pathname, int flags,
-				     mode_t mode)
+				       mode_t mode)
 {
 	return open(pathname, flags, mode);
 }
@@ -914,12 +914,12 @@ void TraceAnalyzer::doLimitedStats()
 
 void TraceAnalyzer::processFtrace()
 {
-	__processGeneric(TRACE_TYPE_FTRACE);
+	processGeneric(TRACE_TYPE_FTRACE);
 }
 
 void TraceAnalyzer::processPerf()
 {
-	__processGeneric(TRACE_TYPE_PERF);
+	processGeneric(TRACE_TYPE_PERF);
 }
 
 void TraceAnalyzer::processAllFilters()
@@ -941,8 +941,8 @@ void TraceAnalyzer::processAllFilters()
 			}
 		}
 		if (OR_filterState.isEnabled(FilterState::FILTER_PID) &&
-		    !__processPidFilter(event, OR_filterPidMap,
-					OR_pidFilterInclusive)) {
+		    !processPidFilter(event, OR_filterPidMap,
+				      OR_pidFilterInclusive)) {
 			filteredEvents.append(eptr);
 			continue;
 		}
@@ -965,8 +965,8 @@ void TraceAnalyzer::processAllFilters()
 			continue;
 		}
 		if (filterState.isEnabled(FilterState::FILTER_PID) &&
-		    __processPidFilter(event, filterPidMap,
-				       pidFilterInclusive)) {
+		    processPidFilter(event, filterPidMap,
+				     pidFilterInclusive)) {
 			continue;
 		}
 		if (filterState.isEnabled(FilterState::FILTER_EVENT) &&

analyzer/traceanalyzer.h
@@ -209,25 +209,25 @@ private:
 				  CPU *eventCPU, int oldpid,
 				  const vtl::Time &oldtime,
 				  int idx);
-	vtl_always_inline void __processSwitchEvent(tracetype_t ttype,
+	vtl_always_inline void processSwitchEvent(tracetype_t ttype,
 						  const TraceEvent &event,
 						  int idx);
-	vtl_always_inline void __processWakeupEvent(tracetype_t ttype,
+	vtl_always_inline void processWakeupEvent(tracetype_t ttype,
 						  const TraceEvent &event,
 						  int idx);
-	vtl_always_inline void __processCPUfreqEvent(tracetype_t ttype,
+	vtl_always_inline void processCPUfreqEvent(tracetype_t ttype,
 						   const TraceEvent &event,
 						   int idx);
-	vtl_always_inline void __processCPUidleEvent(tracetype_t ttype,
+	vtl_always_inline void processCPUidleEvent(tracetype_t ttype,
 						   const TraceEvent &event,
 						   int idx);
-	vtl_always_inline void __processMigrateEvent(tracetype_t ttype,
+	vtl_always_inline void processMigrateEvent(tracetype_t ttype,
 						   const TraceEvent &event,
 						   int idx);
-	vtl_always_inline void __processForkEvent(tracetype_t ttype,
+	vtl_always_inline void processForkEvent(tracetype_t ttype,
 						const TraceEvent &event,
 						int idx);
-	vtl_always_inline void __processExitEvent(tracetype_t ttype,
+	vtl_always_inline void processExitEvent(tracetype_t ttype,
 						const TraceEvent &event,
 						int idx);
 	void addCpuFreqWork(unsigned int cpu,
@@ -240,7 +240,7 @@ private:
 	void processSchedAddTail();
 	void processFreqAddTail();
 	unsigned int guessTimePrecision();
-	vtl_always_inline void __processGeneric(tracetype_t ttype);
+	vtl_always_inline void processGeneric(tracetype_t ttype);
 	vtl_always_inline void updateMaxCPU(unsigned int cpu);
 	vtl_always_inline void updateMaxFreq(unsigned int freq);
 	vtl_always_inline void updateMinFreq(unsigned int freq);
@@ -250,9 +250,9 @@ private:
 	void processPerf();
 	void processAllFilters();
 	vtl_always_inline
-		bool __processPidFilter(const TraceEvent &event,
-					QMap<int, int> &map,
-					bool inclusive);
+		bool processPidFilter(const TraceEvent &event,
+				      QMap<int, int> &map,
+				      bool inclusive);
 	WorkQueue processingQueue;
 	WorkQueue scalingQueue;
 	WorkQueue statsQueue;
@@ -471,9 +471,9 @@ vtl_always_inline Task *TraceAnalyzer::findTask(int pid)
 }
 
 vtl_always_inline
-void TraceAnalyzer::__processMigrateEvent(tracetype_t ttype,
-					  const TraceEvent &event,
-					  int /* idx */)
+void TraceAnalyzer::processMigrateEvent(tracetype_t ttype,
+					const TraceEvent &event,
+					int /* idx */)
 {
 	Migration m;
 	unsigned int oldcpu;
@@ -498,7 +498,7 @@ void TraceAnalyzer::__processMigrateEvent(tracetype_t ttype,
 	migrations.append(m);
 }
 
-vtl_always_inline void TraceAnalyzer::__processForkEvent(tracetype_t ttype,
+vtl_always_inline void TraceAnalyzer::processForkEvent(tracetype_t ttype,
 						       const TraceEvent &event,
 						       int idx)
 {
@@ -529,7 +529,7 @@ vtl_always_inline void TraceAnalyzer::__processForkEvent(tracetype_t ttype,
 	}
 }
 
-vtl_always_inline void TraceAnalyzer::__processExitEvent(tracetype_t ttype,
+vtl_always_inline void TraceAnalyzer::processExitEvent(tracetype_t ttype,
 						       const TraceEvent &event,
 						       int /* idx */)
 {
@@ -553,9 +553,9 @@ vtl_always_inline void TraceAnalyzer::__processExitEvent(tracetype_t ttype,
 }
 
 vtl_always_inline
-void TraceAnalyzer::__processSwitchEvent(tracetype_t ttype,
-					 const TraceEvent &event,
-					 int idx)
+void TraceAnalyzer::processSwitchEvent(tracetype_t ttype,
+				       const TraceEvent &event,
+				       int idx)
 {
 	sched_switch_handle_t handle;
 	unsigned int cpu = event.cpu;
@@ -771,9 +771,9 @@ out:
 }
 
 vtl_always_inline
-void TraceAnalyzer::__processWakeupEvent(tracetype_t ttype,
-					 const TraceEvent &event,
-					 int /* idx */)
+void TraceAnalyzer::processWakeupEvent(tracetype_t ttype,
+				       const TraceEvent &event,
+				       int /* idx */)
 {
 	int pid;
 	Task *task;
@@ -807,9 +807,9 @@ void TraceAnalyzer::__processWakeupEvent(tracetype_t ttype,
 }
 
 vtl_always_inline
-void TraceAnalyzer::__processCPUfreqEvent(tracetype_t ttype,
-					  const TraceEvent &event,
-					  int /* idx */)
+void TraceAnalyzer::processCPUfreqEvent(tracetype_t ttype,
+					const TraceEvent &event,
+					int /* idx */)
 {
 	unsigned int cpu;
 	unsigned int freq;
@@ -839,9 +839,9 @@ void TraceAnalyzer::__processCPUfreqEvent(tracetype_t ttype,
 }
 
 vtl_always_inline
-void TraceAnalyzer::__processCPUidleEvent(tracetype_t ttype,
-					  const TraceEvent &event,
-					  int /* idx */)
+void TraceAnalyzer::processCPUidleEvent(tracetype_t ttype,
+					const TraceEvent &event,
+					int /* idx */)
 {
 	unsigned int cpu;
 	double time;
@@ -895,7 +895,7 @@ vtl_always_inline void TraceAnalyzer::updateMinIdleState(int state)
 		minIdleState = state;
 }
 
-vtl_always_inline void TraceAnalyzer::__processGeneric(tracetype_t ttype)
+vtl_always_inline void TraceAnalyzer::processGeneric(tracetype_t ttype)
 {
 	int i;
 	bool eof = false;
@@ -920,26 +920,26 @@ vtl_always_inline void TraceAnalyzer::__processGeneric(tracetype_t ttype)
 			updateMaxCPU(event.cpu);
 			switch (event.type) {
 			case CPU_FREQUENCY:
-				__processCPUfreqEvent(ttype, event, i);
+				processCPUfreqEvent(ttype, event, i);
 				break;
 			case CPU_IDLE:
-				__processCPUidleEvent(ttype, event, i);
+				processCPUidleEvent(ttype, event, i);
 				break;
 			case SCHED_MIGRATE_TASK:
-				__processMigrateEvent(ttype, event, i);
+				processMigrateEvent(ttype, event, i);
 				break;
 			case SCHED_SWITCH:
-				__processSwitchEvent(ttype, event, i);
+				processSwitchEvent(ttype, event, i);
 				break;
 			case SCHED_WAKEUP:
 			case SCHED_WAKEUP_NEW:
-				__processWakeupEvent(ttype, event, i);
+				processWakeupEvent(ttype, event, i);
 				break;
 			case SCHED_PROCESS_FORK:
-				__processForkEvent(ttype, event, i);
+				processForkEvent(ttype, event, i);
 				break;
 			case SCHED_PROCESS_EXIT:
-				__processExitEvent(ttype, event, i);
+				processExitEvent(ttype, event, i);
 				break;
 			default:
 				break;
@@ -959,9 +959,9 @@ vtl_always_inline void TraceAnalyzer::__processGeneric(tracetype_t ttype)
 }
 
 vtl_always_inline
-bool TraceAnalyzer::__processPidFilter(const TraceEvent &event,
-				       QMap<int, int> &map,
-				       bool inclusive)
+bool TraceAnalyzer::processPidFilter(const TraceEvent &event,
+				     QMap<int, int> &map,
+				     bool inclusive)
 {
 	sched_switch_handle sw_handle;
 	DEFINE_FILTER_PIDMAP_ITERATOR(iter);

mm/stringpool.h
@@ -66,7 +66,7 @@
 #define STRINGPOOL_MAX(A, B) ((A) >= (B) ? A:B)
 #define STRINGPOOL_MIN(A, B) ((A) < (B) ? A:B)
 
-class __DummySP {};
+class DummySP_ {};
 
 class PoolBundleSP {
 public:
@@ -77,9 +77,9 @@ public:
 template<typename HashFunc>
 class StringPool;
 
-#define __STRINGPOOL_ITERATOR(name) \
-vtl::AVLTree<TString, __DummySP, vtl::AVLBALANCE_USEPOINTERS, \
-AVLAllocatorSP<TString, __DummySP>, AVLCompareSP<TString>>::iterator
+#define STRINGPOOL_ITERATOR_(name) \
+vtl::AVLTree<TString, DummySP_, vtl::AVLBALANCE_USEPOINTERS, \
+AVLAllocatorSP<TString, DummySP_>, AVLCompareSP<TString>>::iterator
 
 template <class T>
 class AVLCompareSP {
@@ -116,8 +116,8 @@ private:
 	PoolBundleSP pools;
 };
 
-#define AVLTREE_SIZE ((int)sizeof(vtl::AVLTree<TString, __DummySP,	 \
-vtl::AVLBALANCE_USEPOINTERS, AVLAllocatorSP<TString, __DummySP>, \
+#define AVLTREE_SIZE ((int)sizeof(vtl::AVLTree<TString, DummySP_,	 \
+vtl::AVLBALANCE_USEPOINTERS, AVLAllocatorSP<TString, DummySP_>, \
 			     AVLCompareSP<TString>>))
 #define TSTRING_PTR_SIZE ((int)sizeof(TString*))
 #define TYPICAL_CACHE_LINE_SIZE (64)
@@ -135,8 +135,8 @@ StringPoolEntry(void *data): cachePtr(nullptr), avlTree(data) {
 protected:
 	char cache[SP_CACHE_SIZE];
 	TString *cachePtr;
-	vtl::AVLTree<TString, __DummySP, vtl::AVLBALANCE_USEPOINTERS,
-		AVLAllocatorSP<TString, __DummySP>, AVLCompareSP<TString>>
+	vtl::AVLTree<TString, DummySP_, vtl::AVLBALANCE_USEPOINTERS,
+		     AVLAllocatorSP<TString, DummySP_>, AVLCompareSP<TString>>
 		avlTree;
 };
 
@@ -197,7 +197,7 @@ const TString *StringPool<HashFunc>::allocString(const TString *str,
 				countReuse[hval]++;
 			return entry->cachePtr;
 		}
-		__STRINGPOOL_ITERATOR(iter) iter =
+		STRINGPOOL_ITERATOR_(iter) iter =
 			entry->avlTree.findInsert(*str, isNew);
 		TString &refStr = iter.key();
 		if (isNew) {
@@ -216,7 +216,7 @@ const TString *StringPool<HashFunc>::allocString(const TString *str,
 		entry = new StringPoolEntry<HashFunc>(&avlPools);
 		hashTable[hval] = entry;
 		deleteList.append(entry);
-		__STRINGPOOL_ITERATOR(iter) iter =
+		STRINGPOOL_ITERATOR_(iter) iter =
 			entry->avlTree.findInsert(*str, isNew);
 		if (cutoff != 0)
 			countAllocs[hval]++;
@@ -253,7 +253,7 @@ StringPool<HashFunc>::StringPool(unsigned int nr_pages, unsigned int hSizeP)
 		hSize = hSizeP;
 
 	entryPages = 2 * hSize *
-		sizeof(vtl::AVLNode<TString, __DummySP>) / 4096;
+		sizeof(vtl::AVLNode<TString, DummySP_>) / 4096;
 	entryPages = STRINGPOOL_MAX(1, entryPages);
 	strPages = 2* hSize * sizeof(TString) / 4096;
 	strPages = STRINGPOOL_MAX(16, strPages);
@@ -263,7 +263,7 @@ StringPool<HashFunc>::StringPool(unsigned int nr_pages, unsigned int hSizeP)
 
 	avlPools.charPool = new MemPool(nr_pages, sizeof(char));
 	avlPools.nodePool = new MemPool(entryPages, sizeof(vtl::AVLNode<TString,
-							   __DummySP>));
+							   DummySP_>));
 	hashTable = new StringPoolEntry<HashFunc>*[hSize];
 	countAllocs = new unsigned int[hSize];
 	countReuse = new unsigned int[hSize];

mm/stringtree.h
@@ -77,9 +77,9 @@ public:
 template<typename HashFunc>
 class StringTree;
 
-#define __STRINGTREE_ITERATOR(name) \
+#define VTL_STRINGTREE_ITERATOR_(name) \
 	vtl::AVLTree<TString, event_t, vtl::AVLBALANCE_USEPOINTERS, \
-AVLAllocatorST<TString, event_t>, AVLCompareST<TString>>::iterator
+	AVLAllocatorST<TString, event_t>, AVLCompareST<TString>>::iterator
 
 template <class T>
 class AVLCompareST {
@@ -192,7 +192,7 @@ event_t StringTree<HashFunc>::searchAllocString(const TString *str,
 
 	if (hashTable[hval] != nullptr) {
 		entry = hashTable[hval];
-		__STRINGTREE_ITERATOR(iter) iter =
+		VTL_STRINGTREE_ITERATOR_(iter) iter =
 			entry->avlTree.findInsert(*str, isNew);
 		if (isNew) {
 			event_t &e = iter.value();
@@ -207,7 +207,7 @@ event_t StringTree<HashFunc>::searchAllocString(const TString *str,
 		entry = new StringTreeEntry<HashFunc>(&avlPools);
 		hashTable[hval] = entry;
 		deleteList.append(entry);
-		__STRINGTREE_ITERATOR(iter) iter =
+		VTL_STRINGTREE_ITERATOR_(iter) iter =
 			entry->avlTree.findInsert(*str, isNew);
 		event_t &e = iter.value();
 		e = newval;

parser/fileinfo.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2018  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+ * Copyright (C) 2018, 2020  Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.
@@ -50,8 +50,8 @@
  *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _FILE_INFO_H
-#define _FILE_INFO_H
+#ifndef FILE_INFO_H
+#define FILE_INFO_H
 
 #include <cstdint>
 
@@ -70,4 +70,4 @@ private:
 	struct stat st;
 };
 
-#endif /* _FILE_INFO_H */
+#endif /* FILE_INFO_H */

commit aa38f4c376b8bde4a1a875b55e5522c6f4b0c4d6

vtl/time.h: Fix detection of integer limits
The detection of limits of various integer types and thereby the
selection of a long enough integer type for timeint_t was broken with
the commit 75ffc72e3cefb821138f489d0441a4eb24a90d2c
"Standardize usage of underscore"

This resulted in some 32-bit platforms selecting a too short (32-bit)
integer type. This change should fix the bug.

I believe that this is an important bugfix, at least for 32-bit
platforms.

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

vtl/time.h
@@ -75,12 +75,12 @@ namespace vtl {
 #define VTL_TIME_USE_INT
 #define VTL_TIME_FMT_STRING_ "%d"
 
-#elif LONG_MAX >= _TIMEINT_REQ && LONG_MIN < -_TIMEINT_REQ
+#elif LONG_MAX >= VTL_TIMEINT_REQ_ && LONG_MIN < -VTL_TIMEINT_REQ_
 #define	VTL_TIME_MILLE_ (1000L)
 #define VTL_TIME_USE_LONG
 #define VTL_TIME_FMT_STRING_  "%ld"
 
-#elif LLONG_MAX >= _TIMEINT_REQ && LLONG_MIN < -_TIMEINT_REQ
+#elif LLONG_MAX >= VTL_TIMEINT_REQ_ && LLONG_MIN < -VTL_TIMEINT_REQ_
 #define	VTL_TIME_MILLE_ (1000LL)
 #define VTL_TIME_USE_LLONG
 #define VTL_TIME_FMT_STRING_  "%lld"