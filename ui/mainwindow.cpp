e60b18f1531750de911665e7dc9b8d834a33a0e8
Viktor Rosendahl <viktor.rosendahl@gmail.com>
2021-05-09 01:59:38 +0200

    MainWindow: Unbreak build
    For some reason, I forgot to check that traceshark still builds when I did
    the previous commit and I had forgotten a semicolon, so I broke the build.
    
    Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -406,7 +406,7 @@ MainWindow::~MainWindow()
 	delete cpuSelectDialog;
 	delete graphEnableDialog;
 	delete regexDialog;
-	delete settingStore
+	delete settingStore;
 
 	vtl::set_error_handler(nullptr);
 	delete errorDialog;
