cunctator/traceshark

commit 3cd388ea9ede311a00cb55b3e8dbd9d9d73c9b5d

EventsModel: Display ftrace event flags if available
This will display ftrace events flags string if it is available. This is
the second part of the support for the ftrace flags. Previously we
implemented the parsing of the flags.

This will show a new flags column in the events view widget.

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

ui/eventsmodel.cpp
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2015-2018, 2020  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+ * Copyright (C) 2015-2018, 2020, 2024
+ * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.
@@ -50,6 +51,8 @@
  *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <cstdio>
+
 #include <QVariant>
 #include <QString>
 #include "ui/eventsmodel.h"
@@ -59,29 +62,66 @@
 
 
 EventsModel::EventsModel(QObject *parent):
-	QAbstractTableModel(parent), events(nullptr), eventsPtrs(nullptr)
+	QAbstractTableModel(parent), has_flag_field(false), events(nullptr),
+	eventsPtrs(nullptr)
 {}
 
 EventsModel::EventsModel(vtl::TList<TraceEvent> *e, QObject *parent):
-	QAbstractTableModel(parent), events(e), eventsPtrs(nullptr)
+	QAbstractTableModel(parent), has_flag_field(false), events(e),
+	eventsPtrs(nullptr)
 {}
 
 void EventsModel::setEvents(vtl::TList<TraceEvent> *e)
 {
 	events = e;
 	eventsPtrs = nullptr;
+	checkFlagField();
 }
 
 void EventsModel::setEvents(vtl::TList<const TraceEvent*> *e)
 {
 	events = nullptr;
 	eventsPtrs = e;
+	checkFlagField();
 }
 
 void EventsModel::clear()
 {
 	events = nullptr;
 	eventsPtrs = nullptr;
+	checkFlagField();
+}
+
+void EventsModel::checkFlagField()
+{
+	int s = getSize();
+
+	has_flag_field = false;
+
+	if (s <= 0)
+		return;
+
+	/*
+	 * We check the first event, an event in the middle and the last event.
+	 * If any of these has a flag field, we add the flag field.
+	 */
+	const TraceEvent &first_event = *getEventAt(0);
+	if (first_event.flagstr != nullptr) {
+		has_flag_field = true;
+		return;
+	}
+
+	const TraceEvent &middle_event = *getEventAt(s / 2);
+	if (middle_event.flagstr != nullptr) {
+		has_flag_field = true;
+		return;
+	}
+
+	const TraceEvent &last_event = *getEventAt(s - 1);
+	if (last_event.flagstr != nullptr) {
+		has_flag_field = true;
+		return;
+	}
 }
 
 int EventsModel::rowCount(const QModelIndex & /* parent */) const
@@ -91,7 +131,11 @@ int EventsModel::rowCount(const QModelIndex & /* parent */) const
 
 int EventsModel::columnCount(const QModelIndex & /* parent */) const
 {
-	return column_to_int(NR_COLUMNS);
+	int nr_col = column_to_int(NR_COLUMNS);
+
+	if (!has_flag_field)
+		nr_col--;
+	return nr_col;
 }
 
 QVariant EventsModel::data(const QModelIndex &index, int role) const
@@ -106,7 +150,14 @@ QVariant EventsModel::data(const QModelIndex &index, int role) const
 		return int(Qt::AlignLeft | Qt::AlignVCenter);
 	} else if (role == Qt::DisplayRole) {
 		int row = index.row();
-		column_t column = int_to_column(index.column());
+		int col = index.column();
+
+		if (!has_flag_field) {
+			if (col >= column_to_int(COLUMN_FLAGS))
+				col++;
+		}
+
+		column_t column = int_to_column(col);
 		int size;
 
 		if (events == nullptr && eventsPtrs == nullptr)
@@ -116,6 +167,8 @@ QVariant EventsModel::data(const QModelIndex &index, int role) const
 			return QVariant();
 
 		const TraceEvent &event = *getEventAt(row);
+
+
 		switch(column) {
 		case COLUMN_TIME:
 			return event.time.toQString();
@@ -126,6 +179,10 @@ QVariant EventsModel::data(const QModelIndex &index, int role) const
 		case COLUMN_CPU:
 			return QString("[") + QString::number(event.cpu) +
 				QString("]");
+		case COLUMN_FLAGS:
+			if (event.flagstr != nullptr)
+				str += QString(event.flagstr->ptr);
+			return str;
 		case COLUMN_TYPE:
 			return QString(event.getEventName()->ptr);
 		case COLUMN_INFO:
@@ -162,7 +219,14 @@ QVariant EventsModel::headerData(int section,
 				 Qt::Orientation orientation,
 				 int role) const
 {
-	column_t column = int_to_column(section);
+	column_t column;
+
+	if (!has_flag_field) {
+		if (section >= column_to_int(COLUMN_FLAGS))
+			section++;
+	}
+
+	column = int_to_column(section);
 	if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
 		switch(column) {
 		case COLUMN_TIME:
@@ -173,11 +237,14 @@ QVariant EventsModel::headerData(int section,
 			return QString(tr("PID(TID)"));
 		case COLUMN_CPU:
 			return QString(tr("CPU"));
+		case COLUMN_FLAGS:
+			return QString(tr("Flags"));
 		case COLUMN_TYPE:
 			return QString(tr("Event type"));
 		case COLUMN_INFO:
 			return QString(tr("Info"));
 		default:
+			printf("%s(column=%d)\n", __FUNCTION__, (int) column);
 			return QString(tr("Error in eventsmodel.cpp"));	
 		}
 	}

ui/eventsmodel.h
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2015-2018, 2020  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+ * Copyright (C) 2015-2018, 2020, 2024
+ * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.
@@ -70,6 +71,7 @@ public:
 		COLUMN_TASKNAME,
 		COLUMN_PID,
 		COLUMN_CPU,
+		COLUMN_FLAGS,
 		COLUMN_TYPE,
 		COLUMN_INFO,
 		NR_COLUMNS
@@ -92,10 +94,12 @@ public:
 	static vtl_always_inline column_t int_to_column(int i);
 	static vtl_always_inline int column_to_int(column_t c);
 private:
+	bool has_flag_field;
 	vtl::TList<TraceEvent> *events;
 	vtl::TList<const TraceEvent*> *eventsPtrs;
 	const TraceEvent* getEventAt(int index) const;
 	int getSize() const;
+	void checkFlagField(void);
 };
 
 vtl_always_inline EventsModel::column_t EventsModel::int_to_column(int i)

parser/ftrace/ftracegrammar.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2015, 2016, 2018-2021
+ * Copyright (C) 2015, 2016, 2018-2021, 2024
  * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of

parser/ftrace/ftracegrammar.h
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2015-2020, 2023  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+ * Copyright (C) 2015-2020, 2023, 2024
+ * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.

parser/traceevent.h
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 /*
  * Traceshark - a visualizer for visualizing ftrace and perf traces
- * Copyright (C) 2014-2021, 2023  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+ * Copyright (C) 2014-2021, 2023, 2024
+ * Viktor Rosendahl <viktor.rosendahl@gmail.com>
  *
  * This file is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.

traceshark.pro
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 #
 #  Traceshark - a visualizer for visualizing ftrace and perf traces
-#  Copyright (C) 2014-2023  Viktor Rosendahl <viktor.rosendahl@gmail.com>
+#  Copyright (C) 2014-2024  Viktor Rosendahl <viktor.rosendahl@gmail.com>
 #
 # This file is dual licensed: you can use it either under the terms of
 # the GPL, or the BSD license, at your option.

commit 057ac495f81941814d14856f04eb87350e1de699

EventsWidget:handleDoubleClick(): Fix bug with incorrect column in event
The previous commit: 3cd388e ("EventsModel: Display ftrace event flags if
 available"), introduced a bug because EventsWidget was not updated
 accordingly.

The eventDoubleClicked has incorrect column information if and only if
there was no flags field, which is the case in all perf traces.

We solve this by adding the necessary translation into the column_to_int()
and int_to_column() functions of the EventsModel class. These functions
add and subtract as necessary so that whenever we convert from one type
to the other, we automagically get the corrected value. The columnt_t
values should generally not be corrected, since they are compared (e.g. in
switch statements to the enum constants), while the column as an integer
refers to position, and this value need to be corrected when one field,
i.e. COLUMN_FLAGS, is not present.

Signed-off-by: Viktor Rosendahl <viktor.rosendahl@gmail.com>

ui/eventsmodel.cpp
@@ -131,11 +131,7 @@ int EventsModel::rowCount(const QModelIndex & /* parent */) const
 
 int EventsModel::columnCount(const QModelIndex & /* parent */) const
 {
-	int nr_col = column_to_int(NR_COLUMNS);
-
-	if (!has_flag_field)
-		nr_col--;
-	return nr_col;
+	return column_to_int(NR_COLUMNS);
 }
 
 QVariant EventsModel::data(const QModelIndex &index, int role) const
@@ -151,11 +147,6 @@ QVariant EventsModel::data(const QModelIndex &index, int role) const
 	} else if (role == Qt::DisplayRole) {
 		int row = index.row();
 		int col = index.column();
-
-		if (!has_flag_field) {
-			if (col >= column_to_int(COLUMN_FLAGS))
-				col++;
-		}
 
 		column_t column = int_to_column(col);
 		int size;
@@ -220,11 +211,6 @@ QVariant EventsModel::headerData(int section,
 				 int role) const
 {
 	column_t column;
-
-	if (!has_flag_field) {
-		if (section >= column_to_int(COLUMN_FLAGS))
-			section++;
-	}
 
 	column = int_to_column(section);
 	if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {

ui/eventsmodel.h
@@ -91,8 +91,8 @@ public:
 	void beginResetModel();
 	void endResetModel();
 	Qt::ItemFlags flags(const QModelIndex &index) const;
-	static vtl_always_inline column_t int_to_column(int i);
-	static vtl_always_inline int column_to_int(column_t c);
+	vtl_always_inline column_t int_to_column(int i) const;
+	vtl_always_inline int column_to_int(column_t c) const;
 private:
 	bool has_flag_field;
 	vtl::TList<TraceEvent> *events;
@@ -102,14 +102,20 @@ private:
 	void checkFlagField(void);
 };
 
-vtl_always_inline EventsModel::column_t EventsModel::int_to_column(int i)
+vtl_always_inline EventsModel::column_t EventsModel::int_to_column(int i) const
 {
+	if (!has_flag_field && i >= ((int)(COLUMN_FLAGS)))
+		i++;
 	return (column_t) i;
 }
 
-vtl_always_inline int EventsModel::column_to_int(EventsModel::column_t c)
+vtl_always_inline int EventsModel::column_to_int(EventsModel::column_t c) const
 {
-	return (int) c;
+	int i = (int) c;
+
+	if (!has_flag_field && i >= ((int)(COLUMN_FLAGS) + 1))
+		i--;
+	return i;
 }
 
 #endif /* EVENTSMODEL_H */

ui/eventswidget.cpp
@@ -245,7 +245,7 @@ int EventsWidget::binarySearch(const vtl::Time &time, int start, int end)
 void EventsWidget::handleDoubleClick(const QModelIndex &index)
 {
 	const TraceEvent &event = *getEventAt(index.row());
-	EventsModel::column_t col = (EventsModel::column_t) index.column();
+	EventsModel::column_t col = eventsModel->int_to_column(index.column());
 	emit eventDoubleClicked(col, event);
 }
 